6081b2260ed7c587d3dd5db2365a371c8fb33cd6
Jeong su Choi <>
2021-06-24 13:48:15 +0900

    찜관련 fragment & viewmodel 생성

--- app/src/main/java/com/example/ablyproject/presentation/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/example/ablyproject/presentation/favorite/FavoriteViewModel.kt
@@ -1,2 +1,13 @@
 package com.example.ablyproject.presentation.favorite
 
+import androidx.lifecycle.ViewModel
+import com.example.ablyproject.data.repository.HomeDataRepository
+import com.example.ablyproject.util.SchedulerProvider
+import javax.inject.Inject
+
+class FavoriteViewModel @Inject constructor(
+    private val repository: HomeDataRepository,
+    private val schedulerProvider: SchedulerProvider
+) : ViewModel()   {
+
+}
