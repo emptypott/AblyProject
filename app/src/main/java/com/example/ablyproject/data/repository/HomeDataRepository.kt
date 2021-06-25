7f2e77dc0db2685764ec6523b6760ba09d1c70b7
Jeong su Choi <>
2021-06-25 21:11:21 +0900

    home repository 상품 추가 함수 추가

--- app/src/main/java/com/example/ablyproject/data/repository/HomeDataRepository.kt
+++ app/src/main/java/com/example/ablyproject/data/repository/HomeDataRepository.kt
@@ -3,12 +3,12 @@ package com.example.ablyproject.data.repository
 import androidx.annotation.CheckResult
 import com.example.ablyproject.data.api.HomeApi
 import com.example.ablyproject.data.api.response.Banner
+import com.example.ablyproject.data.api.response.Favorite
 import com.example.ablyproject.data.api.response.Good
 import com.example.ablyproject.data.db.HomeDatabase
 import com.example.ablyproject.util.SchedulerProvider
 import io.reactivex.Completable
 import io.reactivex.Flowable
-import io.reactivex.Single
 import javax.inject.Inject
 
 class HomeDataRepository @Inject constructor(
@@ -26,10 +26,29 @@ class HomeDataRepository @Inject constructor(
             .subscribeOn(schedulerProvider.io()).toCompletable()
     }
 
+    @CheckResult
+    override fun addGoodData(lastId : Int): Completable {
+        return api.getAddGoodData(lastId)
+            .doOnSuccess { response ->
+                homeDataBase.saveAddGoods(response)
+            }
+            .subscribeOn(schedulerProvider.io()).toCompletable()
+    }
+
     override val banners: Flowable<List<Banner>> =
         homeDataBase.getAllBanner().toBanners().filter { it.isNotEmpty() }
 
     override val goods: Flowable<List<Good>> =
         homeDataBase.getAllGood().toGoods().filter { it.isNotEmpty() }
 
+    override val favorites : Flowable<List<Favorite>> =
+        homeDataBase.getAllFavorite().toFavorites().filter{it.isNotEmpty()}
+
+    override fun saveId(favoriteId : Int) {
+        homeDataBase.saveOneFavorite(favoriteId)
+    }
+
+    override fun deleteId(favoriteId: Int) {
+        homeDataBase.deleteOneFavorite(favoriteId)
+    }
 }
