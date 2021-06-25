081147184c4e0cc0da7fca3e7820e9b26f615172
Jeong su Choi <>
2021-06-25 21:16:33 +0900

    appdatabase favorite dao 함수 추가

--- app/src/main/java/com/example/ablyproject/data/db/AppDatabase.kt
+++ app/src/main/java/com/example/ablyproject/data/db/AppDatabase.kt
@@ -2,21 +2,23 @@ package com.example.ablyproject.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
 import com.example.ablyproject.data.db.dao.BannerDao
+import com.example.ablyproject.data.db.dao.FavoriteDao
 import com.example.ablyproject.data.db.dao.GoodDao
 import com.example.ablyproject.data.db.entity.BannerEntity
+import com.example.ablyproject.data.db.entity.FavoriteEntity
 import com.example.ablyproject.data.db.entity.GoodEntity
-
 
 @Database(
     entities = [
         (BannerEntity::class),
-        (GoodEntity::class)
+        (GoodEntity::class),
+        (FavoriteEntity::class)
     ],
-    version = 1
+    version = 3
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun bannerDao() : BannerDao
     abstract fun goodDao() : GoodDao
+    abstract fun favoriteDao() : FavoriteDao
 }
