424499dd2d0222919aa94cb18c62f84cab443ac5
Jeong su Choi <>
2021-06-25 21:14:34 +0900

    homedatabase interface 함수 추가

--- app/src/main/java/com/example/ablyproject/data/db/HomeDatabase.kt
+++ app/src/main/java/com/example/ablyproject/data/db/HomeDatabase.kt
@@ -3,6 +3,7 @@ package com.example.ablyproject.data.db
 import androidx.annotation.CheckResult
 import com.example.ablyproject.data.api.response.Response
 import com.example.ablyproject.data.db.entity.BannerEntity
+import com.example.ablyproject.data.db.entity.FavoriteEntity
 import com.example.ablyproject.data.db.entity.GoodEntity
 import io.reactivex.Flowable
 
@@ -14,5 +15,14 @@ interface HomeDatabase {
     fun getAllGood() : Flowable<List<GoodEntity>>
 
     fun save(response: Response)
+
+    fun saveOneFavorite(favoriteId: Int)
+
+    fun deleteOneFavorite(favoriteId: Int)
+
+    fun saveAddGoods(response: Response)
+
+    @CheckResult
+    fun getAllFavorite() : Flowable<List<FavoriteEntity>>
 
 }
