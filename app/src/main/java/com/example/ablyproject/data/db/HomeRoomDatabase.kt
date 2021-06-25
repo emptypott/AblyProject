c6522507b136f1e2d374bfdf145b2d9958dd1c09
Jeong su Choi <>
2021-06-25 21:10:38 +0900

    database 함수들 추가

--- app/src/main/java/com/example/ablyproject/data/db/HomeRoomDatabase.kt
+++ app/src/main/java/com/example/ablyproject/data/db/HomeRoomDatabase.kt
@@ -2,12 +2,12 @@ package com.example.ablyproject.data.db
 
 import androidx.annotation.CheckResult
 import androidx.room.RoomDatabase
-import com.example.ablyproject.data.api.response.Response
-import com.example.ablyproject.data.api.response.toBannerEntities
-import com.example.ablyproject.data.api.response.toGoodEntities
+import com.example.ablyproject.data.api.response.*
 import com.example.ablyproject.data.db.dao.BannerDao
+import com.example.ablyproject.data.db.dao.FavoriteDao
 import com.example.ablyproject.data.db.dao.GoodDao
 import com.example.ablyproject.data.db.entity.BannerEntity
+import com.example.ablyproject.data.db.entity.FavoriteEntity
 import com.example.ablyproject.data.db.entity.GoodEntity
 import io.reactivex.Flowable
 import javax.inject.Inject
@@ -15,7 +15,8 @@ import javax.inject.Inject
 class HomeRoomDatabase @Inject constructor(
     private val database : RoomDatabase,
     private val bannerDao: BannerDao,
-    private val goodDao: GoodDao
+    private val goodDao: GoodDao,
+    private val favoriteDao: FavoriteDao
 ) : HomeDatabase  {
 
     @CheckResult
@@ -30,4 +31,21 @@ class HomeRoomDatabase @Inject constructor(
             goodDao.clearAndInsert(response.goods.toGoodEntities())
         }
     }
+
+    override fun saveAddGoods(response:Response) {
+        database.runInTransaction {
+            goodDao.insert(response.goods.toGoodEntities())
+        }
+    }
+
+    override fun saveOneFavorite(favoriteId: Int) {
+        val favoriteEntity = FavoriteEntity(favoriteId)
+        favoriteDao.insert(favoriteEntity)
+    }
+
+    override fun deleteOneFavorite(favoriteId: Int) {
+        favoriteDao.delete(favoriteId)
+    }
+
+    override fun getAllFavorite(): Flowable<List<FavoriteEntity>> = favoriteDao.getAllFavorites()
 }
