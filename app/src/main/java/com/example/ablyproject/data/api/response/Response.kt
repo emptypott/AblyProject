5730af621a57d520ed718287797c9ca785ca2d14
Jeong su Choi <>
2021-06-25 21:12:24 +0900

    코드정리

--- app/src/main/java/com/example/ablyproject/data/api/response/Response.kt
+++ app/src/main/java/com/example/ablyproject/data/api/response/Response.kt
@@ -1,8 +1,5 @@
 package com.example.ablyproject.data.api.response
 
-import com.example.ablyproject.data.api.response.Banner
-import com.example.ablyproject.data.api.response.Good
-
 data class Response(
     val banners : List<Banner>?,
     val goods : List<Good>?
