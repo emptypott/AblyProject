0240eddc299b22f2e2f43045abfbf6e3894277ca
Jeong su Choi <>
2021-06-25 21:13:40 +0900

    homerepository interface 함수 추가

--- app/src/main/java/com/example/ablyproject/data/repository/HomeRepository.kt
+++ app/src/main/java/com/example/ablyproject/data/repository/HomeRepository.kt
@@ -2,17 +2,22 @@ package com.example.ablyproject.data.repository
 
 import androidx.annotation.CheckResult
 import com.example.ablyproject.data.api.response.Banner
+import com.example.ablyproject.data.api.response.Favorite
 import com.example.ablyproject.data.api.response.Good
 import io.reactivex.Completable
 import io.reactivex.Flowable
-import io.reactivex.Single
 
 interface HomeRepository {
 
     val banners: Flowable<List<Banner>>
     val goods : Flowable<List<Good>>
+    val favorites : Flowable<List<Favorite>>
 
     @CheckResult fun refreshHomeData() : Completable
-//    @CheckResult fun favorite(good : Good) : Single<Boolean>
 
+    @CheckResult fun addGoodData(lastId : Int) : Completable
+
+    fun saveId(favoriteId : Int)
+
+    fun deleteId(favoriteId : Int)
 }
